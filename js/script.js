1f0234a85606e9f9f03ec94336daa85404e026a8
darile00 <dado.darile00@gmail.com>
2024-02-01 16:08:40 +0100

    fine

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function openNav() {
+    document.getElementById("myExample").style.width = "100%";
+    }
+    
+    function closeNav() {
+    document.getElementById("myExample").style.width = "0%";
+    }
+
+    function openNav1(){
+        document.getElementById("myExample1").style.width = "100%"
+    }
+
+    function closeNav1(){
+        document.getElementById("myExample1").style.width = "0%"
+    }
+
+    
